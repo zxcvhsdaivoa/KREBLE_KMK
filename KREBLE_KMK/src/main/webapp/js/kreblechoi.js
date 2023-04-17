20f3a73f225841221c207d78be7fa8ddfdc3e67c
Administrator <Administrator@501-13>
2023-04-17 15:12:08 +0900

    comm

--- KREBLE_KMK/src/main/webapp/js/kreblechoi.js
+++ KREBLE_KMK/src/main/webapp/js/kreblechoi.js
@@ -1,5 +1,5 @@
 $(function(){
-	var default_no=1;
+	var listfield=1;
 	
 	const urlParams = new URLSearchParams(window.location.search);
 	const field_no = urlParams.get('field_id');
@@ -14,21 +14,21 @@ $(function(){
 	$('.local ul li').click(function(){
 		$('.local ul li').removeClass('on');
 		$(this).addClass("on");
-		if($('.favorite_stadium > ul li:nth-child('+ default_no +')').hasClass('select')==true) {
+		if($('.favorite_stadium > ul li:nth-child('+ listfield +')').hasClass('select')==true) {
 			$('.select a').text($(this).find('a').text())
-			$('.favorite_stadium > ul .default:nth-child('+ default_no +')').addClass('favorite');
+			$('.favorite_stadium > ul .default:nth-child('+ listfield +')').addClass('favorite');
 			$('.favorite_stadium > ul .favorite').removeClass('select');
 			$('.favorite_stadium > ul .favorite').removeClass('default');
-			default_no+=1;
+			listfield+=1;
 			
-			console.log(default_no);
+			console.log(listfield);
 		}
 		else {
 			//$('ul.local_stadium > li > ul > li').click(function(){
 			// 같은 위치에 클릭이벤트가 2개 걸려있어서 더블클릭해야 밑에 코드가 실행 됐던거 이 코드를 지워야 한번 클릭시
 			// 정보가 바뀜
-			   var field_num=$(this).attr('id');
-			    location.href="fieldInfo.choi?field_id="+field_num;
+			   var fieldlistfield=$(this).attr('id');
+			    location.href="fieldInfo.choi?field_id="+fieldlistfield;
 		}
 	});
 	
@@ -42,42 +42,57 @@ $(function(){
 	});
 
 	$('#plusbu').click(function(){
-		var result= confirm("로그인이 필요한 서비스입니다.\n로그인 페이지로 이동하시겠습니까?")
-		if (result==true) {
-			location.replace("login.jsp");
+
+	    $.ajax({
+		    type : "POST",
+		    url : "login_check.jsp",
+		    async: false,
+		    success :function(re){
+		        login_id=re.trim();
+		    },
+		    error:function(e){   
+		        alert(e.responseText); 
+		    }
+		});
+		
+		if(login_id=="null"){
+			var result= confirm("로그인이 필요한 서비스입니다.\n로그인 페이지로 이동하시겠습니까?")
+			if (result==true) {
+				location.replace("login.jsp");
+			}
+			else if(result==false){
+				
+			}
 		}
-		else if(result==false){
-			
-		}
-		else{
-			if (default_no==5){
-				console.log(default_no);
+		else if(login_id==!"null"){
+			if (listfield==5){
+				console.log(listfield);
 				alert('더이상 추가할수 없습니다');
 			}
 			else {
-				console.log(default_no);
-				$('.favorite_stadium > ul li:nth-child('+ default_no +')').addClass('select');
+				console.log(listfield);
+				$('.favorite_stadium > ul li:nth-child('+ listfield +')').addClass('select');
+				}
 			}
-		}
-	
-	})
+		
+	});
 	$('#minusbu').click(function(){
-		if (default_no==1){
-			console.log(default_no);
+		if (listfield==1){
+			console.log(listfield);
 			alert('더이상 제거할수 없습니다');
 		}
 		else {
-			console.log(default_no);
-			if(default_no==5) {
-				default_no=4;
+			console.log(listfield);
+			if(listfield==5) {
+				listfield=4;
 			}
 			else {
-				default_no-=1;
+				listfield-=1;
 			}
-			console.log(default_no);
-			console.log(default_no);
-			$('.favorite_stadium > ul li:nth-child('+ default_no +')').addClass('delete');
-			$('.delete a').text("구장"+default_no);
+			console.log(listfield);
+			console.log(listfield);
+			$('.favorite_stadium > ul li:nth-child('+ listfield +')').addClass('delete');
+			$('.delete a').text("구장"+listfield);
 			$('.delete').addClass('default');
 			$('.delete').removeClass('favorite');
 			$('.delete').removeClass('delete');

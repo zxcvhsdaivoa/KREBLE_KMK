934ae20b60fdc2b82043f3e85aa451e9d70cecf4
Administrator <Administrator@501-13>
2023-04-18 18:22:26 +0900

    favorite commm

---
+++ KREBLE_KMK/src/main/java/vo/field_save_Data.java
@@ -0,0 +1,49 @@
+package vo;
+
+public class field_save_Data {
+	int save_num;
+	String user_id;
+	String save1;
+	String save2;
+	String save3;
+	String save4;
+	
+	public int getSave_num() {
+		return save_num;
+	}
+	public void setSave_num(int save_num) {
+		this.save_num = save_num;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getSave1() {
+		return save1;
+	}
+	public void setSave1(String save1) {
+		this.save1 = save1;
+	}
+	public String getSave2() {
+		return save2;
+	}
+	public void setSave2(String save2) {
+		this.save2 = save2;
+	}
+	public String getSave3() {
+		return save3;
+	}
+	public void setSave3(String save3) {
+		this.save3 = save3;
+	}
+	public String getSave4() {
+		return save4;
+	}
+	public void setSave4(String save4) {
+		this.save4 = save4;
+	}
+	
+	
+}
